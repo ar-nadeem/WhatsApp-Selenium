aad4c580a1312d5da4a3ec9fe48470e56c0e3c49
AbdulRahman Nadeem <ar-nadeem@outlook.com>
2023-04-16 01:56:15 +0500

    Initial Code
    Message parser needs some fixing. Isn't careful for all cases.

---
+++ WhatsExtract.py
@@ -0,0 +1,234 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+
+import csv
+import os
+import sys
+import time
+
+print("User data will be saved in: {}".format(
+    os.path.join(sys.path[0], "UserData")))
+
+
+class WhatsExtract:
+    def __init__(self, executable_path=None, silent=False, headless=False):
+        self.options = webdriver.ChromeOptions()
+        if silent:
+            self.__addOption("--log-level=3")
+        if headless:
+            self.__addOption("--headless")
+
+        self.__addOption(
+            "user-data-dir={}".format(os.path.join(sys.path[0], "UserData")))
+
+        if executable_path:
+            self.browser = webdriver.Chrome(
+                options=self.options, executable_path=executable_path)
+        else:
+            self.browser = webdriver.Chrome(options=self.options)
+
+    def test(self):
+        self.browser.get('https://www.google.com')
+        print(self.browser.title)
+
+    def __addOption(self, option):
+        self.options.add_argument(option)
+
+    def login(self):
+        self.browser.get('https://web.whatsapp.com')
+        if not self.__isLogin():
+            print("Please scan the QR code")
+            while not self.__isLogin():
+                pass
+            print("Login successful")
+        else:
+            print("Already logged in")
+
+    def getChats(self):
+        self.__wait("two")
+        chats = self.browser.find_elements(By.CLASS_NAME, "_11JPr")
+        for chat in chats:
+            if (chat.text == ""):
+                continue
+            print(chat.text)
+
+    def __search(self, query):
+        self.browser.find_element(
+            By.CLASS_NAME, "copyable-text").send_keys(query)
+        return self.__wait("matched-text")
+
+    def __openChat(self, q):
+        # Get the first chat from search results
+        self.__search(q).click()
+
+    def __getChatName(self):
+        return self.browser.find_element(By.CLASS_NAME, "_2rlF7").text
+
+    def __scrollToTop(self, e):
+        e[0].click()
+
+    def __scrollToBottom(self, e):
+        pass
+
+# Scroll up waiting for DOM to not change anymore. Means we reached the top
+# Incremental waits of 1 and 3 seconds are used to avoid false positives
+    def __sendPageUP(self, count):
+        for i in range(count):
+            ActionChains(self.browser).send_keys(Keys.PAGE_UP).perform()
+            time.sleep(0.1)
+
+    def __scrollToView(self, e):
+        ee = self.browser.find_elements(By.CLASS_NAME, e)
+        ee[0].click()
+        while True:
+            self.__sendPageUP(5)
+            oldPage = self.browser.page_source
+            if oldPage == self.browser.page_source:
+                time.sleep(1)
+                self.__sendPageUP(10)
+                oldPage = self.browser.page_source
+                if oldPage == self.browser.page_source:
+                    time.sleep(3)
+                    self.__sendPageUP(10)
+                    oldPage = self.browser.page_source
+                    if oldPage == self.browser.page_source:
+                        break
+
+    def getMessages(self, chatName, all=False):
+        self.__openChat(chatName)
+        self.__wait("_7GVCb")
+
+        if all:
+            self.__scrollToView("_7GVCb")
+
+        time.sleep(3)
+        messages = self.browser.find_elements(By.CLASS_NAME, "_7GVCb")
+
+        fetchedMessages = []
+        for message in messages:
+            msg = self.__parseMessage(message)
+            fetchedMessages.append(msg)
+            print(msg)
+            print("------------------")
+
+        self.__saveToCSV(fetchedMessages, self.__getChatName())
+
+    def getMessagesOutgoing(self, chatName, all=False):
+        self.__openChat(chatName)
+        self.__wait("message-out")
+
+        if all:
+            self.__scrollToView("message-out")
+
+        time.sleep(3)
+        messages = self.browser.find_elements(By.CLASS_NAME, "message-out")
+
+        fetchedMessages = []
+        for message in messages:
+            msg = self.__parseMessage(message)
+            fetchedMessages.append(msg)
+            print(msg)
+            print("------------------")
+
+        self.__saveToCSV(fetchedMessages, self.__getChatName())
+
+    def getMessagesIncomming(self, chatName, all=False):
+        self.__openChat(chatName)
+        self.__wait("message-in")
+
+        if all:
+            self.__scrollToView("message-in")
+
+        time.sleep(3)
+        messages = self.browser.find_elements(By.CLASS_NAME, "message-in")
+
+        fetchedMessages = []
+        for message in messages:
+            msg = self.__parseMessage(message)
+            fetchedMessages.append(msg)
+            print(msg)
+            print("------------------")
+
+        self.__saveToCSV(fetchedMessages, self.__getChatName())
+
+    def __parseMessage(self, message):
+        try:
+            msg = message.find_element(
+                By.CLASS_NAME, "_21Ahp").text
+        except:
+            msg = "MEDIA"
+
+        try:
+            repliedTo = message.find_element(By.CLASS_NAME, "_3IzYj").text
+            repliedMsg = message.find_element(
+                By.CLASS_NAME, "quoted-mention._11JPr").text
+        except:
+            try:
+                repliedTo = message.find_element(By.CLASS_NAME, "_3IzYj").text
+                repliedMsg = message.find_element(
+                    By.CLASS_NAME, "quoted-mention._11JPr").text
+            except:
+                repliedTo = "NONE"
+                repliedMsg = "NONE"
+
+        date = message.find_element(By.CLASS_NAME, "l7jjieqr").text
+
+        # Try to get sender name if none means private chat
+        try:
+            # Other person name in group chat
+            msgSender = message.find_element(
+                By.CLASS_NAME, "_3FuDI.ajgl1lbb.edeob0r2._11JPr").text
+        except:
+            if "message-out" in message.get_attribute("class"):
+                msgSender = "You"  # My name in private chat and group chat
+            else:
+                # Other person name in Private chat | Also the chat title
+                msgSender = self.__getChatName()
+
+        if msg == "":
+            msg = "Emoji"
+        return (date, msgSender, msg, repliedTo, repliedMsg)
+
+    def __isLogin(self):
+        try:
+            # Landing Page (Login QR Code page)
+            self.browser.find_element(By.CLASS_NAME, "landing-wrapper")
+        except:
+            try:
+                # Logged in (Chat list)
+                self.browser.find_element(By.CLASS_NAME, "two")
+                return True
+            except:
+                time.sleep(3)
+                self.__isLogin()
+        return False
+
+    def __wait(self, cName, timeout=60):
+        print("Waiting for element: {}".format(cName),
+              " To load, timeout: {}".format(timeout), " seconds remaining")
+        wait = WebDriverWait(self.browser, timeout)
+        try:
+            element = wait.until(
+                EC.presence_of_element_located((By.CLASS_NAME, cName)))
+        except:
+            exit("Element not found")
+        return element
+
+    def __saveToCSV(self, data, name):
+        fileName = name + ".csv"
+        with open(fileName, 'w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow(("Date", "Sender", "Message",
+                            "Replied To", "Replied Message"))
+            writer.writerows(data)
+            # for item in data:
+            #     writer.writerows(item[0], item[1], item[2], item[3])
+
+
+bot = WhatsExtract(silent=True, headless=False)
+bot.login()
+bot.getMessages("Bawa", all=True)
---
+++ test.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import os
+import sys
+import csv
+
+
+# options = webdriver.ChromeOptions()
+# options.add_argument(
+#     "user-data-dir={}".format(os.path.join(sys.path[0], "UserData")))
+# browser = webdriver.Chrome(options=options)
+
+
+# browser.get('https://www.google.com')
